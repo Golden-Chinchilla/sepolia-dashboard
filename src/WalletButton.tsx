6cc90eb20f7392d24bc646b8101ebf3a1f14167e
David <gaojieli2020@gmail.com>
2025-08-25 22:09:54 +0800

    connect button update

--- src/WalletButton.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export function WalletButton() {
-    return (
-        <button className='absolute top-4 right-4 w-32 h-12 bg-amber-900'>
-            Wallet Button
-        </button>
-    )
-}
